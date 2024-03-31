b577b0271091bef8e96ab05d0bab8a71fc87c4ce
banma1234 <banma1234@gmail.com>
2024-03-31 18:12:41 +0900

    build: 초기설정

--- README.md
+++ README.md
@@ -1,46 +1,20 @@
-# Getting Started with Create React App
+# 초패스트 동시편집 todo-list (feat. yorkie)
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## how to use
 
-## Available Scripts
+CRA로 생성된 react-project 입니다. 해당 프로젝트에서 사용하는 라이브러리의 `api-key`만 발급해주시면 곧바로 사용가능합니다.
 
-In the project directory, you can run:
+해당내용 관련해서는 아래 링크를 참고해주세요.
 
-### `npm start`
+> [[React] 4드론보다 빠른 동시편집 구현하기 (feat: yorkie)](https://chocoham.dev/posts/32)
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+<br/>
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+api-key 발급 이후에는 해당 프로젝트 클론해주시고 필요한 모듈 설치 후 바로 실행해보실 수 있습니다.
 
-### `npm test`
+```
+git clone my-app https://github.com/banma1234/superFast_CRDT
+npm i
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+npm start
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,170 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import yorkie, { DocEventType, Document, JSONArray } from "yorkie-js-sdk";
+import { useState, useEffect } from "react";
+import { displayPeers, createRandomPeers } from "./handlePeers";
+import { Todo } from "./type";
+
+/**
+ * `ENV` = api 요청 관련 환경변수 객체
+ * @enum {string} ENV
+ */
+const ENV = {
+  url: process.env.REACT_APP_YORKIE_API_ADR!,
+  apiKey: process.env.REACT_APP_YORKIE_API_KEY!,
+};
+
+/**
+ * `defaultContent` =  불러온 document가 비어있을 경우 적용할 `초기 컨텐츠`
+ * @enum {{id: number, content: string}} defaultContent
+ */
+const defaultContent = [
+  { id: 0, content: "initial Content" },
+  { id: 1, content: "hello yorkie world" },
+];
 
 function App() {
+  /**
+   * text = 사용자 입력 텍스트
+   * todo = 실제로 document에 삽입될 todo item
+   * peers = 문서에 참여한 사용자
+   * doc = 사용자가 작업할 실제 문서(document)
+   */
+  const [text, setText] = useState<string>("");
+  const [todo, setTodo] = useState<Todo[]>([]);
+  const [peers, setPeers] = useState<string[]>([]);
+  const [doc] = useState<Document<{ todo: JSONArray<Todo> }>>(
+    // "doTest"라는 이름의 새로운 문서 생성
+    () => new yorkie.Document("production"),
+  );
+
+  /**
+   * `actions` = 삽입, 삭제 등 사용자가 `document`에 가할 액션
+   */
+  const actions = {
+    /**
+     * 입력받은 todo의 `텍스트`를 적합한 id값과 함께 yorkie의 `document`에 삽입
+     * @param {string} todo - todo 텍스트
+     * @returns {void}
+     */
+    addTodo(todo: string): void {
+      doc.update(root => {
+        /** 삽입될 todo의 index값 */
+        const index =
+          root.todo.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1;
+
+        root.todo.push({
+          id: index,
+          content: todo,
+        });
+        setText("");
+      });
+    },
+
+    /**
+     * 삭제할 todo의 `id(=index)`를 전달받아 비교를 통해 document에서 제거
+     * @param {number} id - todo의 `index`값
+     * @returns {void}
+     */
+    deleteTodo(id: number): void {
+      doc.update(root => {
+        let target = undefined;
+
+        for (const item of root.todo) {
+          if (item.id === id) {
+            target = item as any;
+            break;
+          }
+        }
+
+        // target이 존재하는 경우 target의 고유 id를 이용해 제거
+        if (target) {
+          root.todo.deleteByID!(target.getID());
+        }
+      });
+    },
+  };
+
+  useEffect(() => {
+    // yorkie client
+    const client = new yorkie.Client(ENV.url, {
+      apiKey: ENV.apiKey,
+    });
+
+    // subscribe document event of "PresenceChanged"(="peers-changed")
+    doc.subscribe("presence", event => {
+      if (event.type !== DocEventType.PresenceChanged) {
+        setPeers(displayPeers(doc.getPresences()));
+      }
+    });
+
+    /**
+     * `document`(=doc)를 구독하고 `이벤트 발생`에 따라 적합한 조치를 취하는 함수
+     * @param {Document<{todo: JSONArray<Todo>}>} doc - 현재 문서의 `document`
+     * @param {(props: any) => void} callback - 호출할 `callback` 함수
+     * @returns {Promise<T>}
+     */
+    async function attachDoc(
+      doc: Document<{ todo: JSONArray<Todo> }>,
+      callback: (props: any) => void,
+    ): Promise<any> {
+      // 01. activate client
+      await client.activate();
+      // 02. attach the document into the client with presence
+      await client.attach(doc, {
+        initialPresence: {
+          userName: createRandomPeers(),
+        },
+      });
+
+      // 03. create default content if not exists.
+      doc.update(root => {
+        if (!root.todo) {
+          root.todo = defaultContent;
+        }
+      }, "create default content if not exists");
+
+      // 04. subscribe doc's change event from local and remote.
+      doc.subscribe(event => {
+        callback(doc.getRoot().todo);
+      });
+
+      // 05. set content to the attached document.
+      callback(doc.getRoot().todo);
+    }
+
+    // doc 업데이트
+    attachDoc(doc, todo => setTodo(todo));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        peers : [
+        {peers.map((man, i) => {
+          return <span key={i}> {man}, </span>;
+        })}{" "}
+        ]
+      </p>
+      <div className="todo">
+        {todo.map((item, i) => {
+          return (
+            <div className="todo__container" key={i}>
+              <p>{item.content}</p>
+              <button onClick={() => actions.deleteTodo(item.id)}>x</button>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
+      <div className="todo__container">
+        <input
+          value={text}
+          onChange={e => {
+            setText(e.target.value);
+          }}
+        />
+        <button onClick={() => actions.addTodo(text)}>제출</button>
+      </div>
     </div>
   );
 }
