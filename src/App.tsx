927185899baf510a4a26a43834b92b6b4fe107b6
banma1234 <banma1234@gmail.com>
2024-12-19 16:53:53 +0900

    fix: todo앱으로 복구

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
 import yorkie, { DocEventType, Document, JSONArray } from "yorkie-js-sdk";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { displayPeers, createRandomPeers } from "./handlePeers";
 import { Todo } from "./type";
+import Main from "./main";
 
 /**
  * `ENV` = api 요청 관련 환경변수 객체
@@ -22,19 +23,18 @@ const defaultContent = [
   { id: 1, content: "hello yorkie world" },
 ];
 
-function App() {
+export default function App() {
   /**
    * text = 사용자 입력 텍스트
    * todo = 실제로 document에 삽입될 todo item
    * peers = 문서에 참여한 사용자
    * doc = 사용자가 작업할 실제 문서(document)
    */
-  const [text, setText] = useState<string>("");
   const [todo, setTodo] = useState<Todo[]>([]);
   const [peers, setPeers] = useState<string[]>([]);
   const [doc] = useState<Document<{ todo: JSONArray<Todo> }>>(
     // "doTest"라는 이름의 새로운 문서 생성
-    () => new yorkie.Document("production")
+    () => new yorkie.Document<{ todo: JSONArray<Todo> }>("REAL_NEW"),
   );
 
   /**
@@ -47,16 +47,18 @@ function App() {
      * @returns {void}
      */
     addTodo(todo: string): void {
-      doc.update((root) => {
+      console.log("Add todo\n");
+      doc.update(root => {
         /** 삽입될 todo의 index값 */
         const index =
           root.todo.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1;
 
+        console.log(index, todo);
         root.todo.push({
           id: index,
           content: todo,
         });
-        setText("");
+        console.log(root.todo);
       });
     },
 
@@ -66,7 +68,7 @@ function App() {
      * @returns {void}
      */
     deleteTodo(id: number): void {
-      doc.update((root) => {
+      doc.update(root => {
         let target = undefined;
 
         for (const item of root.todo) {
@@ -78,10 +80,16 @@ function App() {
 
         // target이 존재하는 경우 target의 고유 id를 이용해 제거
         if (target) {
+          console.log("target : ", target.getID());
           root.todo.deleteByID!(target.getID());
         }
       });
     },
+  };
+
+  const doTest = (damn: any) => {
+    setTodo(damn);
+    console.log(todo);
   };
 
   useEffect(() => {
@@ -90,12 +98,12 @@ function App() {
       apiKey: ENV.apiKey,
     });
 
-    // subscribe document event of "PresenceChanged"(="peers-changed")
-    doc.subscribe("presence", (event) => {
-      if (event.type !== DocEventType.PresenceChanged) {
-        setPeers(displayPeers(doc.getPresences()));
-      }
-    });
+    // // subscribe document event of "PresenceChanged"(="peers-changed")
+    // doc.subscribe("presence", event => {
+    //   if (event.type !== DocEventType.PresenceChanged) {
+    //     setPeers(displayPeers(doc.getPresences()));
+    //   }
+    // });
 
     /**
      * `document`(=doc)를 구독하고 `이벤트 발생`에 따라 적합한 조치를 취하는 함수
@@ -105,68 +113,49 @@ function App() {
      */
     async function attachDoc(
       doc: Document<{ todo: JSONArray<Todo> }>,
-      callback: (props: any) => void
+      callback: (props: any) => void,
     ): Promise<any> {
       // 01. activate client
       await client.activate();
       // 02. attach the document into the client with presence
-      await client.attach(doc, {
-        initialPresence: {
-          userName: createRandomPeers(),
-        },
-      });
+      await client.attach(doc);
 
       // 03. create default content if not exists.
-      doc.update((root) => {
+      doc.update(root => {
         if (!root.todo) {
           root.todo = defaultContent;
+        } else {
         }
       }, "create default content if not exists");
 
       // 04. subscribe doc's change event from local and remote.
-      doc.subscribe((event) => {
+      doc.subscribe(event => {
         callback(doc.getRoot().todo);
       });
 
       // 05. set content to the attached document.
       callback(doc.getRoot().todo);
+      console.log("wow\n", doc.getRoot().todo, "\n");
     }
 
     // doc 업데이트
-    attachDoc(doc, (todo) => setTodo(todo));
+    attachDoc(doc, todo => {
+      console.log("todo : ", todo);
+      setTodo(todo);
+    });
   }, []);
 
   return (
     <div className="App">
-      <p>
+      <button onClick={() => console.log(doc.getRoot().todo)}>wow</button>
+      {/* <p>
         peers : [
         {peers.map((man, i) => {
           return <span key={i}> {man}, </span>;
         })}{" "}
         ]
-      </p>
-      <div className="todo">
-        {todo.map((item, i) => {
-          return (
-            <div className="todo__container" key={i}>
-              <p>{item.content}</p>
-              <button onClick={() => actions.deleteTodo(item.id)}>x</button>
-            </div>
-          );
-        })}
-      </div>
-      <hr />
-      <div className="todo__container">
-        <input
-          value={text}
-          onChange={(e) => {
-            setText(e.target.value);
-          }}
-        />
-        <button onClick={() => actions.addTodo(text)}>제출</button>
-      </div>
+      </p> */}
+      <Main todo={todo} actions={actions} />
     </div>
   );
 }
-
-export default App;
---
+++ src/main.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import { Todo } from "./type";
+
+export default function Main(props: {
+  todo: Todo[];
+  actions: { [name: string]: Function };
+}) {
+  const [text, setText] = useState<string>("");
+  const { todo, actions } = props;
+
+  const submitTodo = () => {
+    console.log("submitTodo!\n");
+    console.log("text : ", text, "\n");
+    actions.addTodo(text);
+    setText("");
+  };
+
+  return (
+    <>
+      <div className="todo">
+        {todo.map((item, i) => {
+          return (
+            <div className="todo__container" key={i}>
+              <p>{item.content}</p>
+              <button onClick={() => actions.deleteTodo(item.id)}>x</button>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
+      <div className="todo__container">
+        <input
+          value={text}
+          onChange={e => {
+            setText(e.target.value);
+          }}
+        />
+        <button onClick={submitTodo}>제출</button>
+      </div>
+    </>
+  );
+}
